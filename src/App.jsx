ec4503e58544547782d8650217c0ec7c68013dd5
sem85cv <sem85cv@gmail.com>
2026-09-24 12:09:43 +0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import ItemsList from './components/ItemsList.jsx'
 import KitsList from './components/KitsList.jsx'
 import ItemSheet from './components/ItemSheet.jsx'
 import KitSheet from './components/KitSheet.jsx'
+import SettingsSheet from './components/SettingsSheet.jsx'
 import { usePersistedState, newId } from './utils/storage.js'
 
 export default function App() {
   const [items, setItems] = usePersistedState('beads_items', [])
   const [kits, setKits] = usePersistedState('beads_kits', [])
+  const [rate, setRate] = usePersistedState('beads_usd_rate', 0)
   const [tab, setTab] = useState('items')
 
   const [itemSheetOpen, setItemSheetOpen] = useState(false)
@@ -18,6 +20,8 @@ export default function App() {
   const [kitSheetOpen, setKitSheetOpen] = useState(false)
   const [editingKitId, setEditingKitId] = useState(null)
 
+  const [settingsOpen, setSettingsOpen] = useState(false)
+
   const editingItem = items.find((i) => i.id === editingItemId) || null
   const editingKit = kits.find((k) => k.id === editingKitId) || null
 
@@ -25,9 +29,9 @@ export default function App() {
   function openEditItem(id) { setEditingItemId(id); setItemSheetOpen(true) }
   function saveItem(data) {
     if (data.id) {
-      setItems((all) => all.map((i) => (i.id === data.id ? data : i)))
+      setItems((all) => all.map((i) => (i.id === data.id ? { ...i, ...data } : i)))
     } else {
-      setItems((all) => [...all, { ...data, id: newId('it') }])
+      setItems((all) => [...all, { ...data, id: newId('it'), createdAt: Date.now() }])
     }
     setItemSheetOpen(false)
   }
@@ -41,9 +45,9 @@ export default function App() {
   function openEditKit(id) { setEditingKitId(id); setKitSheetOpen(true) }
   function saveKit(data) {
     if (data.id) {
-      setKits((all) => all.map((k) => (k.id === data.id ? data : k)))
+      setKits((all) => all.map((k) => (k.id === data.id ? { ...k, ...data } : k)))
     } else {
-      setKits((all) => [...all, { ...data, id: newId('kit') }])
+      setKits((all) => [...all, { ...data, id: newId('kit'), createdAt: Date.now() }])
     }
     setKitSheetOpen(false)
   }
@@ -55,17 +59,18 @@ export default function App() {
 
   return (
     <div className="app">
-      <Header />
+      <Header onSettings={() => setSettingsOpen(true)} />
       <Tabs active={tab} onChange={setTab} />
 
       {tab === 'items'
         ? <ItemsList items={items} onEdit={openEditItem} onDelete={deleteItem} />
-        : <KitsList kits={kits} items={items} onEdit={openEditKit} onDelete={deleteKit} />}
+        : <KitsList kits={kits} items={items} rate={rate} onEdit={openEditKit} onDelete={deleteKit} />}
 
       <button className="fab" type="button" onClick={tab === 'items' ? openNewItem : openNewKit}>+</button>
 
       <ItemSheet open={itemSheetOpen} item={editingItem} onSave={saveItem} onClose={() => setItemSheetOpen(false)} />
       <KitSheet open={kitSheetOpen} kit={editingKit} items={items} onSave={saveKit} onClose={() => setKitSheetOpen(false)} />
+      <SettingsSheet open={settingsOpen} rate={rate} onSave={setRate} onClose={() => setSettingsOpen(false)} />
     </div>
   )
 }
